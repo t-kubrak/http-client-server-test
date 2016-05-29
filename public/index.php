9f74e17ac36fcdff95864b6c11cb7e3a181ba955
t-kubrak <chikashikaari@gmail.com>
2016-05-28 23:57:50 -0400

    Content-Length - Message body

--- public/index.php
+++ public/index.php
@@ -12,24 +12,24 @@ $protocolVersion = substr($_SERVER['SERVER_PROTOCOL'], 5);
 $statusCode = http_response_code();
 $reasonPhrase =  "";
 
-header('Date: '. date("D, d M Y H:i:s T"));
-header('Server: '. $_SERVER['SERVER_NAME']);
-header('Last-Modified: ' . date("D, d M Y H:i:s T"));
-header('Content-Length' . '');
-header('Content-Type: application/json');
+$messageBody = array('@id' => 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+    "to" => "Pillr",
+    "subject" => "Hello Pillr",
+    "message" => "Here is my submission.",
+    "from" => "Taras Kubrak",
+    "timeSent" => date("Y-m-d H:i:s"));
 
 $headers = array('Date' => date("D, d M Y H:i:s T"),
                 'Server' => $_SERVER['SERVER_NAME'],
                 'Last-Modified' => date("D, d M Y H:i:s T"),
-                'Content-Length' => '',
+                'Content-Length' => strlen(json_encode($messageBody, JSON_PRETTY_PRINT)),
                 'Content-Type' => 'application/json');
 
-$messageBody = array('@id' => 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
-                    "to" => "Pillr",
-                    "subject" => "Hello Pillr",
-                    "message" => "Here is my submission.",
-                    "from" => "Taras Kubrak",
-                    "timeSent" => date("Y-m-d H:i:s"));
+header('Date: '. date("D, d M Y H:i:s T"));
+header('Server: '. $_SERVER['SERVER_NAME']);
+header('Last-Modified: ' . date("D, d M Y H:i:s T"));
+header('Content-Length' . strlen(json_encode($messageBody, JSON_PRETTY_PRINT)));
+header('Content-Type: application/json');
 
 $httpResponse = new HttpResponse($protocolVersion, $statusCode, $reasonPhrase, $headers, $messageBody);
 
